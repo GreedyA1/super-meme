d127ed7d69441c1b97a4d398485a232c0c88a6da
GreedyAi <m.vato122@gmail.com>
2021-10-30 12:03:58 +0400

    first commit

---
+++ src/main/java/com/example/supermeme/SuperMemeApplication.java
@@ -0,0 +1,13 @@
+package com.example.supermeme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperMemeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperMemeApplication.class, args);
+	}
+
+}
