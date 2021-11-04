de312cff8821eeef9f2b46ccce6b73774a1effe0
GreedyAi <m.vato122@gmail.com>
2021-11-04 20:27:26 +0400

    add dummy tests.

--- src/test/java/com/example/supermeme/SuperMemeApplicationTests.java
+++ src/test/java/com/example/supermeme/SuperMemeApplicationTests.java
@@ -3,11 +3,18 @@ package com.example.supermeme;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class SuperMemeApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
+	@Test
+	void contextLoads2() {
+		assertTrue(true);
+	}
 }
